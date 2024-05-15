75d32d9bf0ea099a301e32dbd07f76d32563e129
Javabjs0 <bijayalaxmip309@gmail.com>
2024-05-15 19:39:37 +0530

    commit 4

---
+++ OneDrive/Desktop/eclipse folder/OperatorProject/src/Operator.java
@@ -0,0 +1,33 @@
+
+public class Operator {
+
+	public static void main(String[] args) {
+		int a,b;
+		a=10;
+		b=3;
+//		System.out.println("a="+a);//10
+//		System.out.println(a--);
+//		System.out.println(--a);
+//		System.out.println(++a);//11
+//		System.out.println(a++);
+//		
+//		System.out.println("b="+b);
+//		System.out.println(++b);
+//		System.out.println(b--);
+//		System.out.println(--b);
+		Test ob=new Test();
+		System.out.println(ob instanceof Test);
+		//System.out.println(a>>b);
+		
+		
+		
+
+	}
+
+}
+class Test
+{
+	int c=10;
+	
+	
+}
